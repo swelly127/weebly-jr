bd58ca3884353a6ef84afa52d1d17578ab9681da
JESSICA SHU <js2634@cornell.edu>
2014-10-23 14:09:41 -0400

    fix for empty pages

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ WEEBLY_TOKEN = "AnwTFbSfRjVcPwmLZa9sdZ5WeTctj4T6"
 GOOGLE_APP_ID = "516087703797-5bqk46635cdvkln0077vslg1ot3rfjpe.apps.googleusercontent.com"
 GOOGLE_APP_SECRET = "1-YwUXZ0rZp_H6l09FFGqKJD"
 
-REDIRECT_URI = "http://127.0.0.1:4000/oauth"
+REDIRECT_URI = "http://weebly-junior.herokuapp.com/oauth"
 
 AUTH_URL = "https://accounts.google.com/o/oauth2/auth",
 TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
